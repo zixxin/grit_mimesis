0badff2ead34941b2303439bee6bbef677e02ada
zixxin <74345861+zixxin@users.noreply.github.com>
2022-10-02 21:01:44 +0900

    titleSection 구현 완료

--- lib/home.dart
+++ lib/home.dart
@@ -165,42 +165,58 @@ Widget TitleSection(BuildContext context) {
                 textAlign: TextAlign.center,
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(top: 20, left: 15),
-              child: const Text(
-                '저축 습관 앱 런칭기',
-                style: TextStyle(
-                    letterSpacing: 0.5,
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 10, left: 15),
-              child: const Text(
-                '160일의 여정을 돌아보며...',
-                style: TextStyle(
-                    letterSpacing: 0.5, color: Color(0xFF929292), fontSize: 12),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.only(top: 130, left: 15),
-              child: DotsIndicator(
-                dotsCount: 4,
-                position: 1,
-                decorator: DotsDecorator(
-                  spacing: const EdgeInsets.all(4.0),
-                  color: const Color(0xFF6E6E6E),
-                  size: const Size.square(5.0),
-                  activeSize: const Size.square(5.0),
-                  activeColor: const Color(0xFFDBF87A),
-                  activeShape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
+            Stack(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(left: 190, top: 50),
+                  child: Image.asset('image/money.png', width: 200),
                 ),
-              ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 20, left: 15),
+                      child: const Text(
+                        '저축 습관 앱 런칭기',
+                        style: TextStyle(
+                            letterSpacing: 0.5,
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10, left: 15),
+                      child: const Text(
+                        '160일의 여정을 돌아보며...',
+                        style: TextStyle(
+                            letterSpacing: 0.5,
+                            color: Color(0xFF929292),
+                            fontSize: 12),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(top: 130, left: 15),
+                      child: DotsIndicator(
+                        dotsCount: 4,
+                        position: 1,
+                        decorator: DotsDecorator(
+                          spacing: const EdgeInsets.all(4.0),
+                          color: const Color(0xFF6E6E6E),
+                          size: const Size.square(5.0),
+                          activeSize: const Size.square(5.0),
+                          activeColor: const Color(0xFFDBF87A),
+                          activeShape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(5.0)),
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
             ),
           ],
         ),
